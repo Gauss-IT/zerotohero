a1a2a4499fec51c9d3eadbf23e3e6bab5ad59224
esadkoska <57966294+esadkoska@users.noreply.github.com>
2020-01-30 17:29:33 +0100

    "User4" Expenses data changed

--- Production-Numus/User4/default/assets/pages/dashboard/analytic-dashboard.js
+++ Production-Numus/User4/default/assets/pages/dashboard/analytic-dashboard.js
@@ -110,50 +110,50 @@ $(document).ready(function() {
             },
             "dataProvider": [{
                 "date": "2020-01-16",
-                "market1": 71,
-                "market2": 75,
-                "sales1": 5,
-                "sales2": 8
+                "market1": 63,
+                "market2": 71,
+                "sales1": 6,
+                "sales2": 7
             }, {
                 "date": "2020-01-17",
-                "market1": 74,
-                "market2": 78,
+                "market1": 69,
+                "market2": 74,
                 "sales1": 4,
                 "sales2": 6
             }, {
                 "date": "2020-01-18",
-                "market1": 78,
-                "market2": 88,
+                "market1": 77,
+                "market2": 84,
                 "sales1": 5,
-                "sales2": 2
+                "sales2": 3
             }, {
                 "date": "2020-01-19",
-                "market1": 85,
-                "market2": 89,
-                "sales1": 8,
-                "sales2": 9
+                "market1": 82,
+                "market2": 87,
+                "sales1": 7,
+                "sales2": 8
             }, {
                 "date": "2020-01-20",
-                "market1": 82,
-                "market2": 89,
-                "sales1": 9,
-                "sales2": 6
+                "market1": 79,
+                "market2": 86,
+                "sales1": 8,
+                "sales2": 5
             }, {
                 "date": "2020-01-21",
-                "market1": 83,
-                "market2": 85,
+                "market1": 81,
+                "market2": 82,
                 "sales1": 3,
                 "sales2": 5
             }, {
                 "date": "2020-01-22",
-                "market1": 88,
-                "market2": 92,
+                "market1": 85,
+                "market2": 91,
                 "sales1": 5,
                 "sales2": 7
             }, {
                 "date": "2020-01-23",
-                "market1": 85,
-                "market2": 90,
+                "market1": 83,
+                "market2": 89,
                 "sales1": 7,
                 "sales2": 6
             }, {
--- Production-Numus/User4/default/assets/pages/dashboard/ecommerce-dashboard.js
+++ Production-Numus/User4/default/assets/pages/dashboard/ecommerce-dashboard.js
@@ -219,46 +219,46 @@ $(document).ready(function() {
             },
             "dataProvider": [{
                 "date": "2020-01-16",
-                "market1": 91,
-                "market2": 75,
-                "sales1": 5,
-                "sales2": 8
+                "market1": 79,
+                "market2": 68,
+                "sales1": 6,
+                "sales2": 7
             }, {
                 "date": "2020-01-17",
-                "market1": 74,
-                "market2": 78,
-                "sales1": 4,
-                "sales2": 6
+                "market1": 71,
+                "market2": 80,
+                "sales1": 5,
+                "sales2": 5
             }, {
                 "date": "2020-01-18",
-                "market1": 78,
-                "market2": 88,
-                "sales1": 5,
-                "sales2": 2
+                "market1": 80,
+                "market2": 89,
+                "sales1": 6,
+                "sales2": 1
             }, {
                 "date": "2020-01-19",
-                "market1": 85,
-                "market2": 89,
-                "sales1": 8,
+                "market1": 89,
+                "market2": 92,
+                "sales1": 8.5,
                 "sales2": 9
             }, {
                 "date": "2020-01-20",
                 "market1": 82,
                 "market2": 89,
-                "sales1": 9,
+                "sales1": 7.2,
                 "sales2": 6
             }, {
                 "date": "2020-01-21",
-                "market1": 83,
+                "market1": 85,
                 "market2": 85,
-                "sales1": 3,
+                "sales1": 4,
                 "sales2": 5
             }, {
                 "date": "2020-01-22",
-                "market1": 78,
-                "market2": 92,
-                "sales1": 5,
-                "sales2": 7
+                "market1": 80,
+                "market2": 95,
+                "sales1": 6,
+                "sales2": 5
             }]
         });
         // Email sent Ends
