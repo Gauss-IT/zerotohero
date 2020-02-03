7659b11a2395c9726c5c036a6ba574bd9f42be7c
Nuri Osmani <osmani.nuri@gmail.com>
2020-02-03 23:36:36 +0100

    Implement button click on enter

--- Production-Numus/UserAdmin/default/assets/js/login.js
+++ Production-Numus/UserAdmin/default/assets/js/login.js
@@ -1,37 +1,53 @@
 var attempt = 3; // Variable to count number of attempts.
-// Below function Executes on click of login button.
 
-function validate(){
-var username = document.getElementById("username").value;
-var password = document.getElementById("password").value;
-if ( username == "admin" && password == "admin123"){
-window.location = "index.html"; // Redirecting to other page.
-return false;
-} else if (username == "anja" && password == "anja123") {  
-window.location = "../../user1/default/index.html"; // Redirecting to other page.
-return false;
+function bindKeyEvents() {
+  document.getElementById("passwordField")
+    .addEventListener("keyup", function (event) {
+      if (event.keyCode === 13) {
+        event.preventDefault();
+        document.getElementById("submit").click();
+      }
+    });
+  document.getElementById("username")
+    .addEventListener("keyup", function (event) {
+      if (event.keyCode === 13) {
+        event.preventDefault();
+        document.getElementById("submit").click();
+      }
+    });
 }
+
+function validate() {
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("passwordField").value;
+  if (username == "admin" && password == "admin123") {
+    window.location = "index.html"; // Redirecting to other page.
+    return false;
+  } else if (username == "anja" && password == "anja123") {
+    window.location = "../../user1/default/index.html"; // Redirecting to other page.
+    return false;
+  }
   else if (username == "markus" && password == "markus123") {
-window.location = "../../user2/default/index.html"; // Redirecting to other page.
-return false;
-}
-else if (username == "bjorn" && password == "bjorn123") {
-window.location = "../../user3/default/index.html"; // Redirecting to other page.
-return false;
-}
-else if (username == "martin" && password == "martin123") {
-window.location = "../../user4/default/index.html"; // Redirecting to other page.
-return false;
-}
-else{
-attempt --;// Decrementing by one.
-alert("You have"+attempt+" attempts left;");
-// Disabling fields after 3 attempts.
-if( attempt == 0){
-document.getElementById("username").disabled = true;
-document.getElementById("password").disabled = true;
-document.getElementById("submit").disabled = true;
-return false;
-}
-}
+    window.location = "../../user2/default/index.html"; // Redirecting to other page.
+    return false;
+  }
+  else if (username == "bjorn" && password == "bjorn123") {
+    window.location = "../../user3/default/index.html"; // Redirecting to other page.
+    return false;
+  }
+  else if (username == "martin" && password == "martin123") {
+    window.location = "../../user4/default/index.html"; // Redirecting to other page.
+    return false;
+  }
+  else {
+    attempt--;// Decrementing by one.
+    alert("You have" + attempt + " attempts left;");
+    // Disabling fields after 3 attempts.
+    if (attempt == 0) {
+      document.getElementById("username").disabled = true;
+      document.getElementById("passwordField").disabled = true;
+      document.getElementById("submit").disabled = true;
+      return false;
+    }
+  }
 }
