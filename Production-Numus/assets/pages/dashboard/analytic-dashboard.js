401b1df13afb1941a411a7fb919b694eb252c438
Nuri Osmani <osmani.nuri@gmail.com>
2020-02-06 13:52:17 +0100

    Revert "Merge branch 'master' of https://github.com/Gauss-IT/zerotohero"
    This reverts commit 4257f2f898995d07afb968b1ae0ba2d283bb0c58, reversing
    changes made to bb554722f591c0148f7293b10c19f6e708233fd8.

--- Production-Numus/assets/pages/dashboard/analytic-dashboard.js
+++ Production-Numus/assets/pages/dashboard/analytic-dashboard.js
@@ -63,10 +63,10 @@ $(document).ready(function() {
                 "lineThickness": 2,
                 "lineColor": "#93be52",
                 "type": "smoothedLine",
-                "title": "My credit score",
+                "title": "Net worth",
                 "useLineColorForBulletBorder": true,
                 "valueField": "market1",
-                "legendValueText": "[[value]]",
+                "legendValueText": "[[value]]M EUR",
                 "balloonText": "[[title]]<br /><b style='font-size: 130%'>[[value]]M EUR</b>"
             }, {
                 "id": "g2",
@@ -80,7 +80,7 @@ $(document).ready(function() {
                 "lineColor": "#FC6180",
                 // "type": "smoothedLine",
                 "dashLength": 5,
-                "title": "Ola's Credit Score",
+                "title": "Credit Score",
                 "useLineColorForBulletBorder": true,
                 "valueField": "market2",
                 "legendValueText": "[[value]]",
