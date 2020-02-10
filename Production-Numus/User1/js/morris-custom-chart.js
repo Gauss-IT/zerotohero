e058e32c294a16ccf64fe7d82ca4d4e12fc80157
Admir Huseini <admir.huseini@3shape.com>
2020-02-10 15:02:13 +0100

    Fix Financial performance chart

--- Production-Numus/User1/js/morris-custom-chart.js
+++ Production-Numus/User1/js/morris-custom-chart.js
@@ -74,44 +74,44 @@ function donutChart() {
 Morris.Bar({
     element: 'morris-bar-chart',
     data: [{
-        y: '2006',
-        a: 100,
-        b: 90,
-        c: 60
+        y: '2014',
+        a: 62,
+        b: 20,
+        c: 18
     }, {
-        y: '2007',
-        a: 75,
-        b: 65,
-        c: 40
-    }, {
-        y: '2008',
+        y: '2015',
         a: 50,
-        b: 40,
-        c: 30
+        b: 24,
+        c: 12
     }, {
-        y: '2009',
-        a: 75,
-        b: 65,
-        c: 40
+        y: '2016',
+        a: 72,
+        b: 18,
+        c: 16
     }, {
-        y: '2010',
-        a: 50,
-        b: 40,
-        c: 30
+        y: '2017',
+        a: 54,
+        b: 28,
+        c: 11
     }, {
-        y: '2011',
-        a: 75,
-        b: 65,
-        c: 40
+        y: '2018',
+        a: 68,
+        b: 22,
+        c: 21
     }, {
-        y: '2012',
-        a: 100,
-        b: 90,
-        c: 40
+        y: '2019',
+        a: 78,
+        b: 15,
+        c: 15
+    }, {
+        y: '2020',
+        a: 48,
+        b: 26,
+        c: 19
     }],
     xkey: 'y',
     ykeys: ['a', 'b', 'c'],
-    labels: ['A', 'B', 'C'],
+    labels: ['Current', 'Savings', 'Housing'],
     barColors: ['#5FBEAA', '#5D9CEC', '#cCcCcC'],
     hideHover: 'auto',
     gridLineColor: '#eef0f2',
